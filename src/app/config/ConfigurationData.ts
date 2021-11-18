17a74398bb58892f7461eb8714da343cc0814227
Juan <juan.andresgiraldo@hotmail.com>
2021-11-17 22:28:24 -0500

    Realizado validación de formularios video 36

---
+++ src/app/config/ConfigurationData.ts
@@ -0,0 +1,6 @@
+export namespace ConfigurationData{
+    export const EMAIL_MIN_LENGHT =5;
+    export const PASSWORD_MIN_LENGHT =8;
+    export const INVALID_FORM_MESSAGE = "Formulario inválido por favor verifique"
+    export const VALID_FORM_MESSAGE = "OK"
+}
--- src/app/modules/security/general/login/login.component.ts
+++ src/app/modules/security/general/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfigurationData } from 'src/app/config/ConfigurationData';
+
+declare const ShowGeneralMessage: any;
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  dataForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private fb:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.FormBuilding();
+  }
+
+  FormBuilding(){
+    this.dataForm = this.fb.group({
+      username: ["", [Validators.required, Validators.email, Validators.minLength(ConfigurationData.EMAIL_MIN_LENGHT)]],
+      password: ["",[Validators.required, Validators.minLength(ConfigurationData.PASSWORD_MIN_LENGHT)]]
+    });
+  }
+
+  Login(){
+    if(this.dataForm.invalid){
+      ShowGeneralMessage(ConfigurationData.INVALID_FORM_MESSAGE);
+    }else{
+      ShowGeneralMessage(ConfigurationData.VALID_FORM_MESSAGE);
+    }
+  }
+
+  get GetDF (){
+    return this.dataForm.controls;
   }
 
 }
