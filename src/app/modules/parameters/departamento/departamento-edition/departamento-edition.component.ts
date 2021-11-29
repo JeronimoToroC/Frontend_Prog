8dffb306ca670a255ad036be67cfb0f2aab332c4
CCGG55 <crisitangilg55@gmail.com>
2021-11-28 19:12:16 -0500

    editar departamento

--- src/app/modules/parameters/departamento/departamento-edition/departamento-edition.component.ts
+++ src/app/modules/parameters/departamento/departamento-edition/departamento-edition.component.ts
@@ -4,9 +4,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ConfigurationData } from 'src/app/config/ConfigurationData';
 import { DepartamentoModel } from 'src/app/models/parameters/departamento.model';
 import { DepartamentoService } from 'src/app/services/parameters/departamento.service';
-
+import { FacultadModel } from 'src/app/models/parameters/facultad.model';
+import { FacultadService } from 'src/app/services/parameters/facultad.service';
 
 declare const ShowGeneralMessage: any;
+declare const InitSelect: any;
 
 @Component({
   selector: 'app-departamento-edition',
@@ -16,23 +18,27 @@ declare const ShowGeneralMessage: any;
 export class DepartamentoEditionComponent implements OnInit {
 
   dataForm: FormGroup = new FormGroup({});
+  facultadList: FacultadModel[] = [];
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private service: DepartamentoService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private facultadService: FacultadService,
   ) { }
 
   ngOnInit(): void {
     this.FormBuilding();
     this.SearchRecord();
+    this.GetDataForSelects();
   }
 
   FormBuilding() {
     this.dataForm = this.fb.group({
       id: ["", [Validators.required]],
       name: ["", [Validators.required]],
+      facultadId: [0, [Validators.required]],
     });
   }
 
@@ -54,11 +60,22 @@ export class DepartamentoEditionComponent implements OnInit {
   SaveRecord(){
     let model = new DepartamentoModel();
     model.name = this.GetDF["name"].value;
-    model.id = this.GetDF["id"].value;
+    model.id = this.GetDF["id"].value;    
+    model.facultadId=this.GetDF["facultadId"].value;
     this.service.EditRecord(model).subscribe({
       next: (data: DepartamentoModel) => {
         ShowGeneralMessage(ConfigurationData.UPDATED_MESSAGE)
         this.router.navigate(["/parameters/departamento-list"]);
+      }
+    });
+  }
+  GetDataForSelects() {
+    this.facultadService.GetRecordList().subscribe({
+      next: (data: FacultadModel[]) => {
+        this.facultadList = data;
+        setTimeout(() => {
+          InitSelect("selFacultad");
+        }, 100);
       }
     });
   }
