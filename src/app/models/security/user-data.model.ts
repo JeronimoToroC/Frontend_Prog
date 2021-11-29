1d262a6af3f1300f324cd6872a4586ac5f085ea7
Jerónimo Toro C <jeronimo.toro.c@gmail.com>
2021-11-29 09:18:45 -0500

    organiza tabla de usuarios

--- src/app/models/security/user-data.model.ts
+++ src/app/models/security/user-data.model.ts
@@ -1,12 +1,16 @@
-export class UserDataModel{
-    _id?: string;
-    name?: string;
-    lastName?: string;
-    document?: string;
-    password?: string;
-    email?: string;
-    cell?: string;
-    rolesId?: string; 
-    foto?: string;
+export class UserDataModel {
+  _id?: string;
+  name?: string;
+  lastName?: string;
+  document?: string;
+  password?: string;
+  email?: string;
+  cell?: string;
+  rolesId?: string;
+  foto?: string;
 }
 
+export class RolesModel {
+  _id?: string;
+  nombre?: string;
+}
--- src/app/modules/security/users/user-list/user-list.component.ts
+++ src/app/modules/security/users/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserDataModel } from 'src/app/models/security/user-data.model';
+import { UserDataModel, RolesModel } from 'src/app/models/security/user-data.model';
 import { UsuariosService } from 'src/app/services/parameters/usuarios.service';
+import { RolesService } from 'src/app/services/parameters/roles.service';
 
 @Component({
   selector: 'app-user-list',
@@ -10,21 +11,50 @@ import { UsuariosService } from 'src/app/services/parameters/usuarios.service';
 export class UserListComponent implements OnInit {
 
   recordList: UserDataModel[] = [];
+  rolesList: RolesModel[] = [];
 
 
-  constructor( 
-    private service: UsuariosService
+  constructor(
+    private service: UsuariosService,
+    private serciveRol: RolesService
   ) { }
 
   ngOnInit(): void {
     this.ShowRecordList();
   }
+  isShow(r: UserDataModel) {
+    if (r.rolesId !== "sin rol") {
+      return false
+    } else {
+      return true
+    }
+  }
 
-  ShowRecordList(){
+  ShowRecordList() {
     this.service.GetRecordList().subscribe({
-      next: (data: UserDataModel[]) => {
-        this.recordList = data;
-      } 
+      next: (userData) => {
+        let userArray: UserDataModel = {}
+        const dataUser: Array<any> = []
+        this.serciveRol.GetRecordList().subscribe({
+          next: (rolesData: RolesModel[]) => {
+            for (let i = 0; i < userData.length; i++) {
+              rolesData.find(e => e._id === userData[i].rolesId && (
+                userArray = {
+                  cell: userData[i].cell,
+                  email: userData[i].email,
+                  lastName: userData[i].lastName,
+                  name: userData[i].name,
+                  password: userData[i].password,
+                  rolesId: e.nombre,
+                  _id: userData[i]._id
+                },
+                dataUser.push(userArray)
+              ))
+              this.recordList = dataUser
+            }
+          }
+        })
+      }
     });
   }
 
