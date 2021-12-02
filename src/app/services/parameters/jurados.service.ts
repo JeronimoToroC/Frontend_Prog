089170c46984ee5d91f401391519ffc6de47b04f
CCGG55 <crisitangilg55@gmail.com>
2021-12-02 17:04:19 -0500

    detaller

--- src/app/services/parameters/jurados.service.ts
+++ src/app/services/parameters/jurados.service.ts
@@ -28,7 +28,7 @@ export class JuradosService {
   SaveRecord(data: JuradosModel): Observable<JuradosModel>{
     return this.http.post<JuradosModel>(`${this.url}/jurados`,
     {
-      nombre: data.nombre
+      name: data.name
     },
     {
       headers: new HttpHeaders({
